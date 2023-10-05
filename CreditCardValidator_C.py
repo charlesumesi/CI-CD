47d632bfc199429e04989c8deb60ae313c378904
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-10-05 22:18:27 +0000

    card_verifier11

--- CreditCardValidator_C.py
+++ CreditCardValidator_C.py
@@ -11,30 +11,31 @@ class CreditCardValidator:
     def __init__(self) -> None:
         pass
 
-    def credit_card_validator(self):
+    def credit_card_validator(self, enter_number: str) -> None:
         
         '''Based on the Luhn formula'''
         '''This version uses https://en.wikipedia.org/wiki/Luhn_algorithm'''
         
         # Entering of credit card number
-        enter_number = input('Enter credit card number (with no spaces) : ')
-        if enter_number == "":
-            quit()
-        else:        
-            # Implementing formula
-            l = [k for k in enter_number]
-            m = l[:-1]
-            n = m[::-1]
-            o = [str(2*int(i)) for i in n[0::2]]
-            p = ''.join(o)
-            q = n[1::2]
+        # enter_number = input('Enter credit card number (with no spaces) : ')
+        # if enter_number == "":
+        #     quit()
+              
+        # Implementing formula
+        l = [k for k in enter_number]
+        m = l[:-1]
+        n = m[::-1]
+        o = [str(2*int(i)) for i in n[0::2]]
+        p = ''.join(o)
+        q = n[1::2]
+        
+        r = sum(list(map(int, p))) + sum(list(map(int, q))) + int(l[-1])
             
-            r = sum(list(map(int, p))) + sum(list(map(int, q))) + int(l[-1])
-                
-            if r%10 == 0: print('The number entered is a valid credit card number.')
-            else: print('The number entered is not a valid credit card number.')
-            del(l,m,n,o,p,q)
+        if r%10 == 0: print('The number entered is a valid credit card number.')
+        else: print('The number entered is not a valid credit card number.')
+        del(l,m,n,o,p,q)
+
 
 if __name__ == '__main__':
     card = CreditCardValidator()
-    card.credit_card_validator()
+    card.credit_card_validator(input('Enter credit card number (with no spaces) : '))
--- main1.py
+++ main1.py
@@ -1,5 +1,10 @@
+import argparse
 import Person_PythonExample1 as person
 import CreditCardValidator_C
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--git", action="store_true", help="For Git push command only")
+args = parser.parse_args()
 
 # Person instance (object)
 han = person.Person('Han', 'Solo', '30', 'man', 'flying spaceships')
@@ -9,6 +14,9 @@ han.full()
 print('Checking that only card_verifier has this message')
 
 if __name__ == "__main__":
-    card = CreditCardValidator_C.CreditCardValidator()
-    card.credit_card_validator()
+    if args.git:
+        print('The card number validator has been bypassed by Git push. (Check the validator locally.)')
+    else:
+        card = CreditCardValidator_C.CreditCardValidator()
+        card.credit_card_validator(input('Enter credit card number (with no spaces) : '))
     
