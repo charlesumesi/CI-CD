b6345e67dedced40d3c52ac15adf1bfbf1f81a47
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-10-05 17:16:02 +0000

    card_verifier3

--- main1.py
+++ main1.py
@@ -1,4 +1,5 @@
 import Person_PythonExample1 as person
+import CreditCardValidator
 
 # Person instance (object)
 han = person.Person('Han', 'Solo', '30', 'man', 'flying spaceships')
@@ -6,3 +7,6 @@ han = person.Person('Han', 'Solo', '30', 'man', 'flying spaceships')
 print(han.just_firstandlast())
 han.full()
 print('Checking that only card_verifier has this message')
+
+card = CreditCardValidator()
+card.credit_card_validator()
