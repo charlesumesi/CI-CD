15a629e5618c975f830379a361dd9e37a62463dd
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2023-10-05 18:12:22 +0000

    card_verifier7

--- CreditCardValidator_C.py
+++ CreditCardValidator_C.py
@@ -18,20 +18,22 @@ class CreditCardValidator:
         
         # Entering of credit card number
         enter_number = input('Enter credit card number (with no spaces) : ')
-        
-        # Implementing formula
-        l = [k for k in enter_number]
-        m = l[:-1]
-        n = m[::-1]
-        o = [str(2*int(i)) for i in n[0::2]]
-        p = ''.join(o)
-        q = n[1::2]
-        
-        r = sum(list(map(int, p))) + sum(list(map(int, q))) + int(l[-1])
+        if enter_number == "":
+            quit()
+        else:        
+            # Implementing formula
+            l = [k for k in enter_number]
+            m = l[:-1]
+            n = m[::-1]
+            o = [str(2*int(i)) for i in n[0::2]]
+            p = ''.join(o)
+            q = n[1::2]
             
-        if r%10 == 0: print('The number entered is a valid credit card number.')
-        else: print('The number entered is not a valid credit card number.')
-        del(l,m,n,o,p,q)
+            r = sum(list(map(int, p))) + sum(list(map(int, q))) + int(l[-1])
+                
+            if r%10 == 0: print('The number entered is a valid credit card number.')
+            else: print('The number entered is not a valid credit card number.')
+            del(l,m,n,o,p,q)
 
 if __name__ == '__main__':
     card = CreditCardValidator()
